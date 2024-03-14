f1fee295a114a12533065c155e1a54b1360f7569
ShubhamRaut187 <raut.shubham4554@gmail.com>
2024-03-14 18:57:38 +0530

    Loading Comp Added

---
+++ src/Components/LoadingComp.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import CircularProgress from '@mui/material/CircularProgress';
+import './Styles/LoadingComp.css'
+
+function LoadingComp({message}) {
+    return (
+        <div className='loadingcomp_main'>
+            <CircularProgress sx={{color:'e78895'}}/>
+            <h2>{message}...</h2>
+        </div>
+    );
+}
+
+export default LoadingComp;
